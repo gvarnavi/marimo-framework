d0540893d685e898526bd8cafd5959cd07d3a6c9
gvarnavi <gvarnavi@mit.edu>
2024-05-27 13:12:41 -0700

    adding initialization indicator

--- src/data/marimo-pyodide-islands-bug.html.py
+++ src/data/marimo-pyodide-islands-bug.html.py
@@ -3,6 +3,6 @@ import asyncio
 from marimo import MarimoIslandGenerator
 
 generator = MarimoIslandGenerator.from_file("src/data-files/marimo-pyodide-islands-bug.py")
-app = asyncio.run(generator.build())
+#app = asyncio.run(generator.build())
 body = generator.render_body()
 print(body)
